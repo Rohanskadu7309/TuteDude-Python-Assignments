921cf2d7e211ee87369a62f621ac16ed52a3beb6
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-02-17 22:17:31 +0530

    added git commands

--- git_setup.py
+++ git_setup.py
@@ -10,4 +10,11 @@ git status
 git add .
 git commit -m "add commit message"
 git push origin main
+
+to change name of brnach : git branch -m master main
+to merge in another brnach : git push -u origin master:main
+
+to set upstream : git push --set-upstream origin main
+
+to create new branch: git checkout -b branch_name
 """
