421eb06c36f374424b1a07e0136be77322f4a681
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-02-17 22:05:49 +0530

    Inial commit

---
+++ git_setup.py
@@ -0,0 +1,13 @@
+"""
+git config --global user.name "Rohanskadu7309"
+git config --global user.email "rohanskadu7309@gmai.com"
+git config --global color.ui auto
+git init
+git remote add origin AddGitRepoHere
+
+git pull origin main
+git status
+git add .
+git commit -m "add commit message"
+git push origin main
+"""
