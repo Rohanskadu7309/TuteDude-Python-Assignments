5d712dbc315818d0cf42b8de8c2cd43d7bbe99d9
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-02-18 10:35:44 +0530

    assignment 1

---
+++ Assignment_1_task_2.py
@@ -0,0 +1,7 @@
+# Task 2: Create a Personalized Greeting
+
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+print()
+
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program.")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TuteDude-Python-Assignments
 TuteDude Python Assignments
+
+# ########## Assignment 1 ########## 
+
+# Task 1: Perform Basic Mathematical Operations
+
+# Task 2: Create a Personalized Greeting
